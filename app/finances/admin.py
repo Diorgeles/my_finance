31138c365cd832966aa76610efb946691dc357ce
Diorgeles <diorgelesdias@gmail.com>
2019-05-30 22:45:30 -0300

    create admin

--- app/finances/admin.py
+++ app/finances/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import ExpenseAndReceive, Category, Account
 
-# Register your models here.
+
+@admin.register(ExpenseAndReceive)
+class ExpenseAndReceiveAdmin(admin.ModelAdmin):
+    '''Admin View for ExpenseAndReceive'''
+
+    list_display = ('__all__')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    '''Admin View for Category'''
+
+    list_display = ('__all__')
+
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    '''Admin View for Account'''
+
+    list_display = ('__all__')
