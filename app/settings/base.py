35e05883963c2990e23631c6200e459da30f57e7
Diorgeles Dias Lima <diorgelesdias@gmail.com>
2019-07-06 18:02:57 -0300

    add heroku config in project

--- app/settings/base.py
+++ app/settings/base.py
@@ -1,4 +1,5 @@
 import os
+import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^x!8-286#k93dwkvzp!$$42s57pszh*l3$k(%ph3dqhc2y!zn@'
@@ -109,3 +110,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'node_modules'),
+]
+
+# HEROKU #
+db_from_env = dj_database_url.config(conn_max_age=500)
+
+DATABASES['default'].update(db_from_env)
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
